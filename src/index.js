0316f6ab2e6fc41fac66ab14dd293b6614c97b01
PavelBogatyrev1989 <34604949+PavelBogatyrev1989@users.noreply.github.com>
2018-02-23 01:40:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,43 @@
 class Sorter {
-  constructor() {
-    // your implementation
+  constructor(value) {
+    this.value = value;
+    this.arr = [];
+    this.comparator = function (a, b) { return a - b; };
   }
 
   add(element) {
-    // your implementation
+    this.arr.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.arr[index];
   }
 
   get length() {
-    // your implementation
+    return this.arr.length;
   }
 
   toArray() {
-    // your implementation
+    return this.arr;
   }
 
   sort(indices) {
-    // your implementation
+
+    indices.sort(this.comparator);
+    if (indices.length > 1) {
+      var mastSorted = [];
+      for (var i = 0; i < indices.length; ++i) {
+        mastSorted.push(this.arr[indices[i]]);
+      }
+      mastSorted.sort(this.comparator);
+      for (var i = 0; i < indices.length; i++) {
+        this.arr[indices[i]] = mastSorted[i];
+      }
+    }
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.comparator = compareFunction;
   }
 }
-
 module.exports = Sorter;
